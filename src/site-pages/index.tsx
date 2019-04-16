7f10018fe78501871873ca0ad77dca106a7d7a79
Jeff Tian <jie.tian@unicareer.com>
2019-04-16 11:42:29 +0800

    router

--- src/site-pages/index.tsx
+++ src/site-pages/index.tsx
@@ -1,14 +1,23 @@
+import { Hello } from "../components/Hello";
+
 require("../common/service-worker");
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { Layout } from "antd";
 const { Header, Footer, Sider, Content } = Layout;
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 ReactDOM.render(
   <div>
     <Header>Header</Header>
-    <Content>Content</Content>
+    <Content>
+      <BrowserRouter basename={process.env.ASSET_PATH}>
+        <Switch>
+          <Route path="/" component={Hello} />
+        </Switch>
+      </BrowserRouter>
+    </Content>
     <Footer>Footer</Footer>
   </div>,
   document.getElementById("main")
