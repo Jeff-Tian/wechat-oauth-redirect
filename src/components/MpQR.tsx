f357a8b8a084238dee787a5562c4fade2e797c3f
Jeff Tian <jeff.tian@outlook.com>
2020-06-09 19:19:06 +0800

    feat: query scan status

--- src/components/MpQR.tsx
+++ src/components/MpQR.tsx
@@ -4,6 +4,7 @@ import {Skeleton, Result} from "antd"
 export const MpQR = () => {
     const [qr, setQr] = useState()
     const [loading, setLoading] = useState(true)
+    const [ticket, setTicket] = useState()
 
     async function fetchQr() {
         const res = await fetch('https://sso.jiwai.win/wechat/mp-qr-url', {
@@ -14,12 +15,29 @@ export const MpQR = () => {
         const {imageUrl, expire_seconds, sceneId, ticket, url} = await res.json()
 
         setQr(imageUrl)
+        setTicket(ticket)
         setLoading(false)
+    }
+
+    async function fetchQrScanStatus() {
+        const res = await fetch(`https://sso.jiwai.win/wechat/mp-qr-scan-status?ticket=${ticket}`, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        console.log(await res.json())
     }
 
     useEffect(() => {
         fetchQr()
     }, ['dontRunAgain'])
+
+    useEffect(() => {
+        if (!!ticket) {
+            fetchQrScanStatus()
+        }
+    }, [ticket])
 
     return <div>
         <Result title="请使用手机微信扫描二维码" subTitle="等待扫描中……">
